53f1cf4f48826b1b3468b98d4ee06500055d22b1
Gustavo Volobueff <volobueffgustavo@gmail.com>
2025-02-18 11:13:19 -0400

    Create task-cli.js

---
+++ task-manager/task-cli.js
@@ -0,0 +1,56 @@
+class Task{
+    constructor(id, desc){
+        this.id = id;
+        this.description = desc;
+        this.status = "todo";
+        this.createdAt = new Date().toLocaleString("pt-br", {timezone: "America/Campo_Grande"});
+        this.updatedAt = this.createdAt;
+    }
+
+    changeStatus(stat){
+        this.status = stat;
+    }
+
+    changeDescription(desc){
+        this.description = desc;
+    }
+}
+
+if(process.argv.at(2) == undefined){
+    console.log("[ERROR] You have to give a command to the task manager.");
+    return;
+}
+
+const command = process.argv.at(2);
+var id_counter = 0;
+
+switch(command){
+    case "add":
+        addTask(process.argv.at(3));
+        break;
+    case "update":
+        updateTask(process.argv.at(3), process.argv.at(4));
+        break;
+    case "delete":
+        deleteTask();
+        break;
+    default:
+        console.log("[ERROR] The command is not valid.");
+        return;
+}
+
+function addTask(desc){
+    let new_task = new Task(++id_counter, desc);
+    json_obj = JSON.stringify(new_task);
+
+    
+    console.log(`Task added successfully (ID: ${id_counter})`);
+}
+
+function updateTask(id, desc){
+    
+}
+
+function deleteTask(){
+    
+}
